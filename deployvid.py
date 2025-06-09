e5c6f7763738397d0249347f87ee6d36691b0c12
diegothebangbang <diegolarios04@gmail.com>
2025-06-09 13:22:50 +0700

    fix

--- deployvid.py
+++ deployvid.py
@@ -1,10 +1,3 @@
-import asyncio
-
-try:
-    asyncio.get_running_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
 import streamlit as st
 from ultralytics import YOLO
 import tempfile
